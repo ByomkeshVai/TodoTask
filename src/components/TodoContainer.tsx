37bf97306d257f7f95b5b8d0ddb6b7a34d8f8787
byomkesh2580 <habibthemerchant@gmail.com>
2024-02-07 19:49:27 +0600

    TodoCardAdded

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,7 +1,11 @@
+import TodoCard from "./TodoCard";
+
 const TodoContainer = () => {
   return (
-    <div>
-      <h1>This is TodoContainer component</h1>
+    <div className="bg-primary-gradient w-full h-full rounded-xl  p-[5px]">
+      <div className="w-full h-full p-5 space-y-3 bg-white rounded-lg">
+        <TodoCard />
+      </div>
     </div>
   );
 };
